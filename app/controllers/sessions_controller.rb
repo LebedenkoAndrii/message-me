6cc016cbf72b3a9f3e928145708fe48f91777a52
LebedenkoAndrii <lebedenko1928@gmail.com>
2025-02-21 17:45:40 +0200

    add success and error messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,17 +6,17 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:session][:username])
       if user && user.authenticate(params[:session][:password])
         session[:user_id] = user.id
-        flash[:success] = "You have successfully logged in"
+        flash[:notice] = "You have successfully logged in"
         redirect_to root_path
       else
         flash.now[:error] = "There was something wrong with your login information"
-        render 'new'
+        render 'new', status: :unprocessable_entity
     end
   end
 
   def destroy
     session[:user_id] = nil
-    flash[:success] = "You have successfully logged out"
+    flash[:notice] = "You have successfully logged out"
     redirect_to login_path
   end
 end
